38a3f498c2496b455633017f1f05d5681a56e008
Jason Chen <90630687+jasonc3805@users.noreply.github.com>
2023-01-10 12:06:52 -0500

    x

--- course-catalog/pages/CourseCatalog.vue
+++ course-catalog/pages/CourseCatalog.vue
@@ -1,24 +1,12 @@
 <script setup lang="ts">
-import SearchButton from '~/components/icons/SearchButton.vue';
+import SearchComponent from '~/components/catalog-search/SearchComponent.vue';
 </script>
 
 <template>
     <div id="courses" class="flex justify-center items-start h-screen px-12">
         <div id="content" class="w-full mt-24 space-y-6">
             <h2 class="text-center text-4xl font-semibold">Courses</h2>
-            <div id="search" class="w-full flex justify-start items-center space-x-4">
-                <select name="sort" id="sort" class="w-40 h-10 p-1 border border-gray-300 bg-white text-gray-400 rounded">
-                    <option>Sort by</option>
-                    <option value="name">Course Name</option>
-                    <option value="subject">Subject</option>
-                    <option value="grade">Grade</option>
-                </select>
-                <div id="search-bar" class="flex justify-center items-center relative">
-                    <input type="text" placeholder="Search" class="border border-gray-300 rounded w-112 h-10 p-2"/>
-                    <SearchButton class="absolute right-3 text-gray-400 cursor-pointer"/>
-                </div>
-            
-            </div>
+            <SearchComponent/>
         </div>
     </div>
 </template>
