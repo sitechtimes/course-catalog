7b6364e0e951c92c19f139b0d26826597885175b
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2023-01-11 15:33:23 -0500

    tabs creation

--- course-catalog/components/schedule builder/Courses.vue
+++ course-catalog/components/schedule builder/Courses.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
+  <div class="folder">
+    <div class="tabs">
+      <div class="tab r">Russian</div>
+      <div class="tab g">Gym</div>
+      <div class="tab e">Electives</div>
+      <div class="tab h">History</div>
+      <div class="tab en">English</div>
+      <div class="tab s">Science</div>
+      <div class="tab m">Math</div>
+    </div>
     <div class="file">
       <div class="landing">
         <h2>Instructions</h2>
@@ -13,6 +22,66 @@
   </div>
 </template>
 <style scoped>
+.r {
+  background-color: #fedcb5;
+}
+.g {
+  background-color: #ffadcb;
+}
+.e {
+  background-color: #fffbd6;
+}
+.h {
+  background-color: #e0d6ff;
+}
+.en {
+  background-color: #d6eeff;
+}
+.s {
+  background-color: #cbf2d4;
+}
+.m {
+  background-color: #ffdfdf;
+}
+.folder {
+  display: flex;
+  flex-direction: column;
+}
+.tabs {
+  margin-left: 35%;
+  display: flex;
+  flex-direction: row;
+}
+.tab {
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  padding-top: 0.5rem;
+  width: 5rem;
+  font-size: 1rem;
+  border-top-right-radius: 1rem;
+  border-top-left-radius: 1rem;
+}
+@media only screen and (min-width: 1440px) {
+  .file {
+    line-height: 4;
+    width: 75%;
+    margin-left: 40%;
+    background-color: #fff2e2;
+    border-radius: 0.8rem;
+    padding: 3rem;
+    height: 35rem;
+  }
+  .landing {
+    display: block;
+  }
+  h2 {
+    font-weight: bold;
+    font-size: 1.7rem;
+  }
+  h3 {
+    font-size: 1.2rem;
+  }
+}
 @media only screen and (min-width: 976px) {
   .file {
     line-height: 4;
@@ -35,23 +104,7 @@
 }
 @media only screen and (min-width: 1440px) {
   .file {
-    line-height: 4;
-    width: 75%;
-    margin-left: 40%;
-    background-color: #fff2e2;
-    border-radius: 0.8rem;
-    padding: 3rem;
-    height: 35rem;
-  }
-  .landing {
-    display: block;
-  }
-  h2 {
-    font-weight: bold;
-    font-size: 1.7rem;
-  }
-  h3 {
-    font-size: 1.2rem;
+    margin-left: 30%;
   }
 }
 </style>
