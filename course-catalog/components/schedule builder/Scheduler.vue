dae49252290beed5eaace24d6643824a99732803
vvwenv <90654380+vvwenv@users.noreply.github.com>
2023-01-11 12:26:24 -0500

    table
    This reverts commit 3892c044f32944b9eaaed3f9f52cbbfe69061475.

--- course-catalog/components/schedule builder/Scheduler.vue
+++ course-catalog/components/schedule builder/Scheduler.vue
@@ -1,13 +1,56 @@
 <template>
-  <table class="border-solid border-4 rounded-lg">
-    <tr>
-      <th>Period</th>
+  <table class="">
+    <tr class="">
+      <th class="border-black">Period</th>
       <th>Class</th>
     </tr>
-    <tr>
-      <td>1</td>
+    <tr class="">
+      <td class="">1</td>
+      <td>Stuff</td>
+    </tr>
+    <tr class="">
+      <td class="">2</td>
+      <td>Stuff</td>
+    </tr>
+    <tr class="">
+      <td class="">3</td>
+      <td>Stuff</td>
+    </tr>
+    <tr class="">
+      <td class="">4</td>
+      <td>Stuff</td>
+    </tr>
+    <tr class="">
+      <td class="">5</td>
+      <td>Stuff</td>
+    </tr>
+    <tr class="">
+      <td class="">6</td>
+      <td>Stuff</td>
+    </tr>
+    <tr class="">
+      <td class="">7</td>
+      <td>Stuff</td>
+    </tr>
+    <tr class="">
+      <td class="">8</td>
+      <td>Stuff</td>
+    </tr>
+    <tr class="">
+      <td class="">9</td>
       <td>Stuff</td>
     </tr>
   </table>
 </template>
-<style scoped></style>
+<style scoped>
+table,
+th,
+td {
+  border: 4px solid lightgrey;
+  border-radius: 0.5rem;
+  text-align: center;
+}
+table {
+  margin-top: 1rem;
+}
+</style>
