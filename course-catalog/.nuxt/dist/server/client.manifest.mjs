6812b0341052928f1676eb11c2e206da8a941868
Hashir <hashirabbasi112@gmail.com>
2023-01-13 14:38:18 -0500

    Delete course-catalog/.nuxt directory

--- course-catalog/.nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/m.frid/Documents/GitHub/course-catalog/course-catalog/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- course-catalog/.nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = string
-declare module "/Users/m.frid/Documents/GitHub/course-catalog/course-catalog/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
