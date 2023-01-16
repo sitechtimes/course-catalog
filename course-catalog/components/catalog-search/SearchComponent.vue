29ad7a7805626a4af00591bde0fe49755982497f
AgnethaDMello <agnethad@nycstudents.net>
2023-01-16 02:31:20 -0500

    ok

--- course-catalog/components/catalog-search/SearchComponent.vue
+++ course-catalog/components/catalog-search/SearchComponent.vue
@@ -124,3 +124,33 @@ function filteredList() {
   );
 }
 </script>
+
+<!--  <template>
+    <div>
+    <div v-bind:class="id">
+      <h2>{{ title }}</h2>
+      <img v-bind:src="image" class="image" alt="" />
+      <p>${{ price }}</p>
+      <button @click="add(index)">Add to Cart!</button>
+      <button @click="remove(index)">Remove from Cart!</button>
+    </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Card",
+    props: {
+      title: String,
+      price: Number,
+      image: String,
+      id: Number,
+      input: String,
+      index: Number,
+      add: Function,
+      remove: Function,
+      filteredList: Function,
+    },
+  };
+  </script>
+   -->
--- course-catalog/pages/CourseCatalog.vue
+++ course-catalog/pages/CourseCatalog.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import SearchComponent from '~/components/catalog-search/SearchComponent.vue';
+import SearchResults from '~/components/catalog-search/SearchResults.vue';
 </script>
 
 <template>
@@ -7,6 +8,7 @@ import SearchComponent from '~/components/catalog-search/SearchComponent.vue';
         <div id="content" class="w-full mt-24 space-y-6">
             <h2 class="text-center text-4xl font-semibold">Courses</h2>
             <SearchComponent/>
+            <SearchResults/>
         </div>
     </div>
 </template>
