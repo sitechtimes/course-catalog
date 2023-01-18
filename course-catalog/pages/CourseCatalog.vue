cafa41df64c54ae0d0960b480b3845c4076db42a
Jason Chen <90630687+jasonc3805@users.noreply.github.com>
2023-01-18 12:21:47 -0500

    Update CourseCatalog.vue

--- course-catalog/pages/CourseCatalog.vue
+++ course-catalog/pages/CourseCatalog.vue
@@ -5,14 +5,12 @@ import CourseCard from "../components/course-cards/CourseCard.vue";
 </script>
 
 <template>
-    <div id="courses" class="flex justify-center items-start h-screen w-full px-12 bg-green-100">
-        <div id="content" class="flex flex-col justify-center items-start w-full mt-24 space-y-6 bg-blue-100">
+    <div id="catalog" class="flex justify-center items-start h-screen w-full px-12">
+        <div id="content" class="flex flex-col justify-center items-center w-full mt-28 space-y-6">
             <h2 class="text-center text-4xl font-semibold">Courses</h2>
             <SearchComponent />
-            <div id="courses" class="flex flex-wrap justify-center items-center bg-red-100 w-full max-w-[100rem]">
-                <div v-for="x in useCourseStore().courses">
-                    <CourseCard :course="x" />
-                </div>
+            <div v-for="x in useCourseStore().courses" id="courses" class="flex flex-wrap justify-center items-center max-w-[80rem]">
+                <CourseCard :course="x" />
             </div>
         </div>
     </div>
