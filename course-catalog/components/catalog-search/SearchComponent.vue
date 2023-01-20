9b900200aa8110e5903530a5817d42eeeda1edc6
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2023-01-20 09:19:23 -0500

    Revert "Merge remote-tracking branch 'origin/catalog-search' into course-cards-fixed"
    This reverts commit 4e9f20e8f976aafc8b4ff36ca43c344c0c9956ce, reversing
    changes made to 14b7d304ab0c8a9054ea37e3d06e7a17ffb96a68.

--- course-catalog/components/catalog-search/SearchComponent.vue
+++ course-catalog/components/catalog-search/SearchComponent.vue
@@ -1,27 +1,18 @@
+<script setup lang="ts">
+import SearchButton from '~/components/icons/SearchButton.vue';
+</script>
+
 <template>
     <div id="search" class="w-full flex justify-center items-center">
-    <select name="sort" id="sort" class=" hidden w-40 h-10 p-1 border border-gray-300 bg-white text-gray-400 mr-4 rounded md:block">
+        <select name="sort" id="sort" class=" hidden w-40 h-10 p-1 border border-gray-300 bg-white text-gray-400 mr-4 rounded md:block">
                     <option>Sort by</option>
                     <option value="name">Course Name</option>
                     <option value="subject">Subject</option>
                     <option value="grade">Grade</option>
                 </select>
-    <div id="search-bar" class="flex justify-center items-center relative">
-        <input :value="modelValue" @input="$emit('update:modelValue', $event?.target?.value)" class="border border-gray-300 rounded w-112 h-10 p-2"/>
-        <SearchButton class="absolute right-3 text-gray-400 cursor-pointer"/>
-    </div>
-</div>
+                <div id="search-bar" class="flex justify-center items-center relative">
+                    <input type="text" placeholder="Search" class="border border-gray-300 rounded w-112 h-10 p-2"/>
+                    <SearchButton class="absolute right-3 text-gray-400 cursor-pointer"/>
+                </div>
+            </div>
 </template>
-
-<script lang="ts">
-import SearchButton from '~/components/icons/SearchButton.vue';
-import Sort from '../catalog-search/Sort.vue';
-export default {
-    components: {
-        SearchButton,
-        Sort
-    },
-    props: ['modelValue'],
-    emits: ['update:modelValue']
-}
-</script>
--- course-catalog/pages/CourseCatalog.vue
+++ course-catalog/pages/CourseCatalog.vue
@@ -1,16 +1,7 @@
-<script lang="ts">
+<script setup lang="ts">
 import { useCourseStore } from "~~/store/store"
 import SearchComponent from "../components/catalog-search/SearchComponent.vue";
 import CourseCard from "../components/course-cards/CourseCard.vue";
-import SearchResults from '../components/catalog-search/SearchResults.vue';
-import Sort from '../components/catalog-search/Sort.vue';
-
-export default {
-    components: {
-        SearchResults,
-        Sort
-    }
-}
 </script>
 
 <template>
@@ -21,7 +12,6 @@ export default {
             <div id="courses" class="flex flex-wrap justify-center items-center max-w-[80rem]">
                 <CourseCard v-for="courses in useCourseStore().courses" :course="courses" />
             </div>
-            <SearchResults/>
         </div>
     </div>
 </template>
