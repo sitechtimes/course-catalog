14b7d304ab0c8a9054ea37e3d06e7a17ffb96a68
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2023-01-20 09:12:28 -0500

    Update CourseCard.vue

--- course-catalog/components/course-cards/CourseCard.vue
+++ course-catalog/components/course-cards/CourseCard.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import CourseType from './CourseType.vue';
 import SubjectType from './SubjectType.vue';
-const type = true
 
 defineProps({
     course: Object
@@ -12,7 +11,7 @@ defineProps({
 <template>
     <div>
         <NuxtLink :to="course?.page">
-            <div id="card" class="bg-zinc-50 w-80 h-auto m-4 px-4 py-3 rounded-lg shadow-sm">
+            <div id="card" class="bg-zinc-50 w-80 h-36 m-4 px-4 py-3 rounded-lg shadow-sm">
                 <h2 id="name" class="text-2xl font-semibold">{{course?.name}}</h2>
                 <div id="grade" class="flex justify-start items-start space-x-1 text-sm font-light text-zinc-500">
                     <p>Grade:</p>
@@ -20,7 +19,7 @@ defineProps({
                 </div>
                 <div class="flex justify-start items-start space-x-2">
                     <SubjectType />
-                    <CourseType v-if="type"/>
+                    <CourseType/>
                 </div>
             </div>
         </NuxtLink>
