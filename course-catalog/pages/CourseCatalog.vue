47f9ba7e01d57c80147e77e09da3d0d54ac64c50
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2023-01-20 09:22:55 -0500

    Revert "Revert "Merge remote-tracking branch 'origin/catalog-search' into course-cards-fixed""
    This reverts commit 9b900200aa8110e5903530a5817d42eeeda1edc6.

--- course-catalog/pages/CourseCatalog.vue
+++ course-catalog/pages/CourseCatalog.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts">
+<script lang="ts">
 import { useCourseStore } from "~~/store/store"
 import SearchComponent from "../components/catalog-search/SearchComponent.vue";
 import CourseCard from "../components/course-cards/CourseCard.vue";
+import SearchResults from '../components/catalog-search/SearchResults.vue';
+import Sort from '../components/catalog-search/Sort.vue';
+
+export default {
+    components: {
+        SearchResults,
+        Sort
+    }
+}
 </script>
 
 <template>
@@ -12,6 +21,7 @@ import CourseCard from "../components/course-cards/CourseCard.vue";
             <div id="courses" class="flex flex-wrap justify-center items-center max-w-[80rem]">
                 <CourseCard v-for="courses in useCourseStore().courses" :course="courses" />
             </div>
+            <SearchResults/>
         </div>
     </div>
 </template>
