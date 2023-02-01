c9b3edce3c33f9b1ce6e9557c67484369eb352af
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2023-02-01 11:57:22 -0500

    stuff

--- course-catalog/components/course-cards/CourseCard.vue
+++ course-catalog/components/course-cards/CourseCard.vue
@@ -16,10 +16,10 @@ defineProps({
 <template>
     <div v-if="course?.catalog == true">
         <NuxtLink :to="`/courses/${course?.id}`">
-            <div id="card" class="bg-zinc-50 w-80 h-36 m-4 px-4 py-3 rounded-lg shadow-sm cursor-pointer">
-                <h2 id="name" class="text-2xl font-semibold h-16">{{course?.name}}</h2>
+            <div id="card" class="bg-zinc-50 w-80 h-36 m-4 px-4 py-[0.6rem] rounded-lg shadow-sm cursor-pointer">
+                <h2 id="name" class="text-2xl font-semibold h-[4.4rem]">{{course?.name}}</h2>
                 <div id="grade" class="flex justify-start items-start space-x-1 text-sm text-zinc-500">
-                    <p class="font-medium pr-1">Grade:</p>
+                    <p class="font-medium pr-1">Grade</p>
                     <p class="font-bold space-x-1">
                         <span v-if="course?.freshman == true">9 </span> 
                         <span v-if="course?.sophomore == true">10 </span> 
