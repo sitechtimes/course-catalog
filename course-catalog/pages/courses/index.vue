3421c3ec7f512c7074b735d289a8c587e149e615
AgnethaDMello <agnethad@nycstudents.net>
2023-02-17 03:07:13 -0500

    ???

--- course-catalog/pages/courses/index.vue
+++ course-catalog/pages/courses/index.vue
@@ -6,10 +6,45 @@ import SearchBar from "../../components/catalog-search/SearchBar.vue";
 import CourseCard from "../../components/course-cards/CourseCard.vue";
 
 let input = ref("")
-function filteredList() {
+const AllCourses = getCourses().courses
+let display = AllCourses
+
+/* function filteredList() {
+  return display.filter((course) =>
+    course.toLowerCase().includes(input.value.toLowerCase())
+  );
+} */
+if (typeof document !== 'undefined') {
+    let searchForm = document.getElementById('search-bar');
+    let searchResultList = document.querySelectorAll('.course-cards');
+    let result = document.getElementById('courses');
+    let searchValue = result.getElementsByTagName("h2");
+
+
+    searchForm.addEventListener("keyup", function () {
+        for (var i = 0; i < searchValue.length; i++) {
+
+            console.log(searchForm.value);
+
+            let value = searchResultList[i].getElementsByTagName('h2')[0];
+
+            let filterValue = value.textContent || value.innerHTML;
+
+            if (filterValue.toUpperCase().indexOf(searchForm.value.toUpperCase()) > -1) {
+                searchResultList[i].style.display = "";
+            }
+            else {
+                searchResultList[i].style.display = "none";
+            }
+        }
+    })
+}
+
+/* function filteredList() {
     if (typeof document !== 'undefined') {
     let input = document.getElementById('search-bar').value
     input=input.toLowerCase();
+
     let x = document.getElementsByClassName('course-name');
       
     for (i = 0; i < x.length; i++) { 
@@ -21,64 +56,48 @@ function filteredList() {
         }
     }
 }
-}
-
-/* function filteredList() {
-    return getCourses().courses.filter((course) => {
-        return course.name.toLowerCase().indexOf(input.toLowerCase()) != -1
-    })
 } */
 
-/* if (typeof document !== 'undefined') {
-   let element = document.querySelector('#hi')
-   element.innerHTML = "hi";
-} */
 
-// export default {
-//     components: {
-//         SearchBar,
-//         SortComponent,
-//         CourseCard
-//     },
-//   data() {
-//     return {
-//             courses: getCourses().courses,
-//             input: ref(""),
-//          }
-//         },
-//         computed: {
-//             filteredList() {
-//                 return this.courses.filter((course) => {
-//                     return course.name.toLowerCase().indexOf(this.input.toLowerCase()) != -1;
-//                 })
-//             }
-//         },
-//         mounted() {
-//             this.filteredList
-//         }
-// }
+/* export default {
+    components: {
+        SearchBar,
+        SortComponent,
+        CourseCard
+    },
+  data() {
+    return {
+            courses: getCourses().courses,
+            input: ref(""),
+         }
+        },
+        computed: {
+            filteredList() {
+                return this.courses.filter((course) => {
+                return course.name.toLowerCase().indexOf(this.input.toLowerCase()) != -1;
+                }
+            )}
+        },
+        mounted() {
+            this.filteredList
+        }
+} */
 </script>
 
 <template>
     <div id="catalog" class="flex justify-center items-start h-screen w-full px-12">
         <div id="content" class="flex flex-col justify-center items-center w-full mt-28 space-y-6">
             <h2 class="text-center text-4xl font-semibold">Courses</h2>
-            
+
             <div id="search" class="w-full flex flex-col justify-center items-center">
-                <div id="search-bar" class="flex justify-center items-center">
-                    <SortComponent/>
-                    <SearchBar onkeyup="filteredList()" class="mb-4" type="text"  placeholder="Search Courses..." />
+                <div id="search-section" class="flex justify-center items-center">
+                    <SortComponent />
+                    <SearchBar id="search-bar" class="mb-4" type="text" v-model="input" placeholder="Search Courses..." />
                 </div>
-                <div id="courses" class="flex flex-wrap justify-center items-center max-w-[80rem] mb-12">
-                    <CourseCard class="course-cards" v-for="course in getCourses().courses" :course="course"/>
-                </div>
-                <div id="error-msg" v-if="input&&!filteredList.length">
-                    <p>No results found!</p>
+                <div id="courses" class="flex flex-wrap justify-center items-center max-w-[80rem]">
+                    <CourseCard class="course-cards" v-for="courses in getCourses().courses" :course="courses" />
                 </div>
             </div>
-            <!-- <div id="courses" class="flex flex-wrap justify-center items-center max-w-[80rem]">
-                <CourseCard v-for="courses in getCourses().courses" :course="courses"/>
-            </div> -->
         </div>
     </div>
 </template>
