af678a5d319982b70bc556cd5fb1a29a380ce08a
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2023-03-06 11:54:27 -0500

    Revert "circle"
    This reverts commit 067bb7ad95fcf7e1ae3191443650787ece042d15.

--- course-catalog/pages/index.vue
+++ course-catalog/pages/index.vue
@@ -30,13 +30,7 @@ function doStuff() {
             View Courses
           </button></NuxtLink
         >
-        <NuxtLink to="/builder"
-          ><button
-            class="bg-secondary-s text-white text-sm sm:text-base w-32 h-10 sm:w-40 sm:h-12 rounded"
-          >
-            Build Schedule
-          </button></NuxtLink
-        >
+        <NuxtLink to="/builder"><button class="bg-secondary-s text-white text-sm sm:text-base w-32 h-10 sm:w-40 sm:h-12 rounded">Build Schedule</button></NuxtLink>
       </div>
     </div>
     <div
@@ -55,12 +49,6 @@ function doStuff() {
   </div>
 </template>
 <style scoped>
-#big {
-  border-radius: 50rem;
-}
-#small {
-  border-radius: 50rem;
-}
 button {
   border-radius: 5px;
 }
