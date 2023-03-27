c3b1fc44c4693014aaf59635e8a9cef9ef03b35b
Hashir <hashirabbasi112@gmail.com>
2023-03-27 12:51:19 -0400

    Revert "attempt to fix hydration issues"
    This reverts commit 8f5149c91834c3073fcc0ad5a043370efc5aefba.

--- course-catalog/pages/courses/index.vue
+++ course-catalog/pages/courses/index.vue
@@ -33,15 +33,18 @@ export default {
     <div id="catalog" class="flex justify-center items-start h-screen w-full px-12">
         <div id="content" class="flex flex-col justify-center items-center w-full mt-28 space-y-6">
             <h2 class="text-center text-4xl font-semibold">Courses</h2>
+
             <div id="search" class="w-full flex flex-col justify-center items-center">
                 <div id="search-section" class="flex justify-center items-center">
                      <SortComponent />
                         <SearchBar class="mb-4" type="text" v-model="input" placeholder="Search Courses..." />
-                </div>
-                <p v-if="input&&!filteredList.length">No results found!</p>
-            </div>
-            <div id="courses" class="flex flex-wrap justify-center items-center max-w-[80rem]">
-                <CourseCard v-for="course in filteredList" :course="course"/>
+</div>
+     <div id="courses" class="flex flex-wrap justify-center items-center max-w-[80rem]">
+    <CourseCard v-for="course in filteredList" :course="course"/>
+    </div>
+   <div class="item error" v-if="input&&!filteredList.length">
+      <p>No results found!</p>
+   </div>
             </div>
         </div>
     </div>
