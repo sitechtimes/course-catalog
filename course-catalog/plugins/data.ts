b43f255326f8e81c144d74cfec4d273ebb2451b4
Harvey Jiang <harveyjiang11@gmail.com>
2023-07-06 08:32:25 -0400

    fix1

--- course-catalog/plugins/data.ts
+++ course-catalog/plugins/data.ts
@@ -1,24 +1,26 @@
-import axios from "axios"
-import { useCourseStore } from "~~/store/store"
-import course from '~~/interface/course'
-import courseNode from '~/interface/course'
-const Course:any = []
-export default async function (){
+import axios from "axios";
+import { useCourseStore } from "~~/store/store";
+import course from "~~/interface/course";
+import courseNode from "~/interface/course";
+// const Course:any = []
+export default async function () {
 
-
-    await axios({
-        url:`http://10.94.168.231:8000/course`,
-        method: 'get',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    }).then((res)=>{
-        console.log(res.data)
-            res.data.forEach((x:any) => {
-                Course.push(x)
-            });
-        useCourseStore().courses=Course
-        })
-    
-
+  const { data, pending, error, refresh } = await useAsyncData("course", () =>
+    $fetch("http://10.94.168.231:8000/course")
+  );
+  useCourseStore().courses = data;
+  
+  // await axios({
+  //     url:`http://10.94.168.231:8000/course`,
+  //     method: 'get',
+  //     headers: {
+  //         'Content-Type': 'application/json',
+  //     },
+  // }).then((res)=>{
+  //     console.log(res.data)
+  //         res.data.forEach((x:any) => {
+  //             Course.push(x)
+  //         });
+  //     useCourseStore().courses=Course
+  //     })
 }
