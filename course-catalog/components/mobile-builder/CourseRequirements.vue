e076744c5bb174926bc82d7575c05a9a268679b8
AgnethaDMello <agnethad@nycstudents.net>
2024-02-05 10:58:57 -0500

    minor change!

--- course-catalog/components/mobile-builder/CourseRequirements.vue
+++ course-catalog/components/mobile-builder/CourseRequirements.vue
@@ -31,7 +31,7 @@ export default {
             <div class="flex gap-2 w-full" v-for="x in Object.keys(requirements)">
                 <svg v-if="requirements[x]" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18"
                     viewBox="0,0,256,256">
-                    <g fill="#38cb63" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                    <g fill="#1abd18" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
                         stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
                         font-family="none" font-weight="none" font-size="none" text-anchor="none"
                         style="mix-blend-mode: normal">
@@ -42,7 +42,7 @@ export default {
                         </g>
                     </g>
                 </svg>
-                <svg v-else xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 24 24">
+                <svg v-else xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 24 24" fill="#f20707">
                     <path
                         d="M 12 2 C 6.4889971 2 2 6.4889971 2 12 C 2 17.511003 6.4889971 22 12 22 C 17.511003 22 22 17.511003 22 12 C 22 6.4889971 17.511003 2 12 2 z M 12 4 C 16.430123 4 20 7.5698774 20 12 C 20 16.430123 16.430123 20 12 20 C 7.5698774 20 4 16.430123 4 12 C 4 7.5698774 7.5698774 4 12 4 z M 8.7070312 7.2929688 L 7.2929688 8.7070312 L 10.585938 12 L 7.2929688 15.292969 L 8.7070312 16.707031 L 12 13.414062 L 15.292969 16.707031 L 16.707031 15.292969 L 13.414062 12 L 16.707031 8.7070312 L 15.292969 7.2929688 L 12 10.585938 L 8.7070312 7.2929688 z">
                     </path>
