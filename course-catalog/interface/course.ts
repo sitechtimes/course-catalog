be761888e2dbfbea4dedfe0f32cdf0b97b3b50a5
EmersonYang3 <emersonyang28@gmail.com>
2025-02-04 14:49:48 -0500

    Switch from API to static JSON data and update course interface

--- course-catalog/interface/course.ts
+++ course-catalog/interface/course.ts
@@ -1,17 +1,20 @@
-export default interface course{
-    name: string
-    freshman: boolean
-    sophomore: boolean
-    junior: boolean
-    senior: boolean
-    ap: boolean
-    courseCode: string
-    doublePeriod: boolean
-    catalog: boolean
-    subject: string
-    id: string
+export default interface course {
+    id: number,
+    name: string,
+    freshman: boolean,
+    sophomore: boolean,
+    junior: boolean,
+    senior: boolean,
+    subject: string,
+    description: string,
+    ap: boolean,
+    honors: boolean,
+    mandatoryCourse: boolean,
+    doublePeriod: boolean,
+    prerequisites: string[],
+    codes: string[]
 }
 
-export default interface courseNode{
-    node:course
+export default interface courseNode {
+    node: course;
 }
